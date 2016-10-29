d203f4b7d64d2db2e2c3fc1bfe5d8192416317f1
slamma009 <alsnurb@gmail.com>
2016-10-29 15:42:34 -0500

    Baby Launcher Added

---
+++ LaunchTheBaby/Assets/Scripts/BabyLauncher.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BabyLauncher : MonoBehaviour {
+    public Rigidbody Baby;
+    public float Force = 1.0f;
+	// Use this for initialization
+	void Start () {
+        Baby.AddForce(new Vector3(1, 1, 0).normalized * Force);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
