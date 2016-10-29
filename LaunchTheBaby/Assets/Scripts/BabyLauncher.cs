97fea714782e56d70d55d42b81477320a4a11644
slamma009 <alsnurb@gmail.com>
2016-10-29 16:04:54 -0500

    Changes to Baby Launcher
    Adds force to all rigidbodies on ragdoll now, instead of just 1 section.

--- LaunchTheBaby/Assets/Scripts/BabyLauncher.cs
+++ LaunchTheBaby/Assets/Scripts/BabyLauncher.cs
@@ -1,16 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class BabyLauncher : MonoBehaviour {
     public Rigidbody Baby;
     public float Force = 1.0f;
 	// Use this for initialization
 	void Start () {
-        Baby.AddForce(new Vector3(1, 1, 0).normalized * Force);
+        Transform parent = Baby.transform.parent;
+        while(parent.parent != null)
+        {
+            parent = parent.parent;
+        }
+
+        parent.rotation = Random.rotation;
+
+        var children = GetAllRigidChildren(Baby.transform);
+
+        foreach(var child in children)
+        {
+            child.AddForce(new Vector3(1, 1, 0).normalized * Force);
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+
+    public List<Rigidbody> GetAllRigidChildren(Transform start)
+    {
+        List<Rigidbody> children = new List<Rigidbody>();
+        Rigidbody rigid = start.GetComponent<Rigidbody>();
+        if (rigid != null)
+        {
+            children.Add(rigid);
+        }
+        foreach (Transform child in start)
+        {
+            children.AddRange(GetAllRigidChildren(child));
+        }
+
+        return children;
+    }
 }
